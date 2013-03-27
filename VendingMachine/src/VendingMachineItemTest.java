f7cb2b6d2f66c770c7c5bd7bbb476f93976b2ca1
Boquist Erika <eboquist@gmail.com>
2013-03-27 18:45:15 -0400

    Commit 1: initial commit with empty test files

---
+++ VendingMachine/src/VendingMachineItemTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author Erika Boquist
+ *
+ * Class for testing vending machine items class
+ */
+public class VendingMachineItemTest {
+	
+	//VendingMachineItem Constructor
+	
+	//String getName()
+	
+	//String getPrice()
+
+}
---
+++ VendingMachine/src/VendingMachineTest.java
@@ -0,0 +1,20 @@
+/**
+ * @author Erika Boquist
+ *
+ * Class for testing vending machine class
+ */
+public class VendingMachineTest {
+	
+	//void addItem(VendingMachineItem item, String code)
+	
+	//VendingMachineItem removeItem(String code)
+	
+	//void insertMoney(double amount)
+	
+	//double getBalance()
+	
+	//boolean makePurchase()
+	
+	//double returnChange()
+
+}
