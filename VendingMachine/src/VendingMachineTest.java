94b468ff752b12ed97a285e92cf961c03ace0470
Boquist Erika <eboquist@gmail.com>
2013-03-27 20:21:30 -0400

    Commit 4:includes jUnit test for VendingMachine.java (addItem)

--- VendingMachine/src/VendingMachineTest.java
+++ VendingMachine/src/VendingMachineTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * @author Erika Boquist
  *
@@ -6,6 +10,48 @@
 public class VendingMachineTest {
 	
 	//void addItem(VendingMachineItem item, String code)
+	/**
+	 * Tests the VendingMachine addItem method with an empty slot.
+	 */
+	@Test
+	public void testVendAddItem(){
+		String testName = "testName";
+		double testPricePos = 1;
+		
+		VendingMachineItem testItem = new VendingMachineItem(testName, testPricePos);
+		VendingMachine testMachine = new VendingMachine();
+		
+		testMachine.addItem(testItem, "A");
+	}
+	/**
+	 * Tests the VendingMachine addItem method with a full slot, should throw exception.
+	 */
+	@Test (expected = VendingMachineException.class)
+	public void testVendAddItemFull() throws Exception{
+		String testName = "testName";
+		double testPricePos = 1;
+		
+		VendingMachineItem testItem = new VendingMachineItem(testName, testPricePos);
+		VendingMachineItem testItem2 = new VendingMachineItem(testName, testPricePos);
+		VendingMachine testMachine = new VendingMachine();
+		
+		testMachine.addItem(testItem, "A");
+		testMachine.addItem(testItem2, "A");
+	}
+	/**
+	 * Tests the VendingMachine addItem method with an empty slot, but incorrect
+	 * slot code, should throw exception.
+	 */
+	@Test (expected = VendingMachineException.class)
+	public void testVendAddItemCode() throws Exception{
+		String testName = "testName";
+		double testPricePos = 1;
+		
+		VendingMachineItem testItem = new VendingMachineItem(testName, testPricePos);
+		VendingMachine testMachine = new VendingMachine();
+		
+		testMachine.addItem(testItem, "F");
+	}
 	
 	//VendingMachineItem removeItem(String code)
 	
